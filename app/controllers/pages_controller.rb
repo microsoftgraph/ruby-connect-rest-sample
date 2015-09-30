fca32c2a17d5cc8dad00c05c651021afb6f1ede7
Brian Melton <brianmel@microsoft.com>
2015-09-29 17:34:27 -0700

    Adding sendmail screen

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -13,6 +13,19 @@ class PagesController < ApplicationController
     code = params[:code]
     puts "Code: #{code}"
     
+    @name = auth_hash.info.name
+    @email = auth_hash.info.email
+
+    puts "Name: #{@name}"
+    puts "Email: #{@email}"
+
     # So, do I need to request an access token, or can ADAL?
+    ##
+    # Take a look at https://github.com/AzureAD/omniauth-azure-activedirectory/blob/21df1d4ba6f6557714164d537e62635993faa335/examples/rails-todo-list-app/app/models/user.rb
+    # since it looks like it handles the requesting of tokens and such
+  end
+
+  def auth_hash
+    request.env['omniauth.auth']
   end
 end
