0be236586f12558c0f4598843e3ee90e64c2e66d
Ricardo Loo <ricardol@microsoft.com>
2015-10-02 10:02:07 -0700

    Since we're controlling the port, I guess it's okay to keep this value as is

--- config/environment.rb
+++ config/environment.rb
@@ -6,7 +6,6 @@
 ENV['CLIENT_ID'] = '748ba382-f304-49f6-94d1-bfca33668f78'
 ENV['CLIENT_SECRET'] = 'RrMy6FTud+m9DgjRGhKN9kssm1zcjHlRrI6sMWxMez0='
 ENV['TENANT'] = 'patsoldemo6.onmicrosoft.com'
-# TODO can I dynamically get the host + port?
 ENV['REPLY_URL'] = 'http://localhost:9292/auth/azureactivedirectory/callback'
 
 ENV['LOGOUT_ENDPOINT'] = 'https://login.microsoftonline.com/common/oauth2/logout'
