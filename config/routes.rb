c932cdd2d86d875caf836cc547e018cc11af0ac3
Brian Melton <brianmel@microsoft.com>
2015-10-06 13:55:06 -0700

    Line wrapping

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,8 @@
 ##
 
 Rails.application.routes.draw do
-  # The priority is based upon order of creation: first created -> highest priority.
+  # The priority is based upon order of creation:
+  # first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
