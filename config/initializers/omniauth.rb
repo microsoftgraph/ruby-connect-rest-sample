e64285b0b2d7a58aa47dea40285895e10817274f
Ricardo Loo <ricardol@microsoft.com>
2015-10-27 08:31:16 -0700

    Updating spacing to adhere to Ruby style guide

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,6 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   # Pass the id of the 'common' tenant (ed46058a-1957-405e-8d5a-fae110f41cb8)
-  provider :azure_activedirectory, ENV['CLIENT_ID'], 'ed46058a-1957-405e-8d5a-fae110f41cb8'
+  provider :azure_activedirectory,
+           ENV['CLIENT_ID'],
+           'ed46058a-1957-405e-8d5a-fae110f41cb8'
 end
