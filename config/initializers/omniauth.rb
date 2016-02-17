c3978e618314d9237b568416d54aff1ffacd3e94
Ricardo Loo <ricardol@microsoft.com>
2016-02-17 14:59:10 -0800

    If ENV['TENANT'] doesn't have the complete tenant, append '.onmicrosoft.com'

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,6 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
+  tenant = if ENV['TENANT'].include? '.' then ENV['TENANT'] else ENV['TENANT'] + '.onmicrosoft.com' end
   provider :azure_activedirectory,
            ENV['CLIENT_ID'],
-           ENV['TENANT']
+           tenant
 end
