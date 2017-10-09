7c5c83d74510ee47985a0ad305510076079c817b
davegregg <davegregg@gmail.com>
2017-10-09 10:11:38 -0700

    Removed Rubocop frozen_string_literal magic comments and replaced with rules in .rubocop.yml

--- config/initializers/omniauth-microsoft-v2-auth.rb
+++ config/initializers/omniauth-microsoft-v2-auth.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 require 'openssl'
 OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   # You can have the root of your site routed with "root"
