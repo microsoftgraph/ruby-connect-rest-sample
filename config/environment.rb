2b754f14f99cdd0549abd5e179b50f51454852e5
davegregg <davegregg@gmail.com>
2017-11-30 12:54:25 -0500

    Satisfy rubocop on long lines

--- config/environment.rb
+++ config/environment.rb
@@ -5,7 +5,12 @@ require_relative 'application'
 # in your Microsoft App Registration Portal entry for your app.
 ENV['CLIENT_ID'] = 'ENTER_YOUR_CLIENT_ID'
 ENV['CLIENT_SECRET'] = 'ENTER_YOUR_SECRET'
-ENV['OAUTH_SCOPE'] = 'openid email profile https://graph.microsoft.com/User.Read https://graph.microsoft.com/Mail.Send'
+ENV['OAUTH_SCOPE'] =
+  'openid ' \
+  'email ' \
+  'profile ' \
+  'https://graph.microsoft.com/User.Read ' \
+  'https://graph.microsoft.com/Mail.Send'
 
 # Initialize the Rails application.
 Rails.application.initialize!
